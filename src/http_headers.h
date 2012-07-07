7f97417d20a88957ea1cc1747172cb65bad51afe
fleuria <me.ssword@gmail.com>
2012-07-07 11:29:02 +0800

    added prototypes for http_headers

--- src/http_headers.h
+++ src/http_headers.h
@@ -1,8 +1,14 @@
 #ifndef HTTP_HEADERS_H
 #define HTTP_HEADERS_H
 
-typedef struct vt_http_headers {
+#include "st.h"
 
+typedef struct vt_http_headers {
+    st_table *hash;
 } vt_http_headers_t;
+    
+void vt_http_headers_init(vt_http_headers_t *hh);
+void vt_http_headers_set(char *key, char *val);
+char* vt_http_headers_get(char *key);
 
 #endif
