68d4400720443f28a57821451a02c6a16e0b16cf
fleuria <me.ssword@gmail.com>
2012-07-10 17:34:50 +0800

    made the fields in conn.h aligned

--- src/conn.h
+++ src/conn.h
@@ -6,12 +6,12 @@ struct vt_request;
 struct vt_response;
 
 typedef struct vt_conn {
-    int state;
-    int fd;
-    struct vt_event event;
-    struct sockaddr_in sockaddr;
-    struct vt_pool *pool;
-    TAILQ_HEAD(, vt_request) requests;
+    int                 state;
+    int                 fd;
+    struct vt_event     event;
+    struct sockaddr_in  sockaddr;
+    struct vt_pool     *pool;
+    TAILQ_HEAD(, vt_request)  requests;
     TAILQ_HEAD(, vt_response) responses;
 } vt_conn_t;
 
